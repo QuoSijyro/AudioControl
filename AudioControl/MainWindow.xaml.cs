9b828f60eda81783f33c161cb60b4c3142bae214
QuoSijyro <sijyro@gmail.com>
2018-05-17 18:39:36 +0200

    Solution crated, first method to set the volume of an application created.

---
+++ AudioControl/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AudioControl
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void SetApplicationVolume(string applicationName, double level)
+        {
+            Process.Start("nircmd.exe", "changeappvolume " + applicationName + " " + level);
+        }
+    }
+}
